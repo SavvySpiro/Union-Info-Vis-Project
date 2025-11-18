c9c9d9f0b9cf49f6fdafe3da5037ddc34cde0b35
nohrg <noah.raegrant@gmail.com>
2025-11-18 10:31:27 -0500

    reorg into folders, added contract to data

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -1,6 +1,6 @@
 from pdf2image import convert_from_path
 
-pages = convert_from_path('assets/Final-package-offer.pdf', dpi=200)
+pages = convert_from_path('data/Final-package-offer.pdf', dpi=200)
 for i, page in enumerate(pages):
     print(f"saving page {i+1}")
     page.save(f"assets/page_{i+1}.png", "PNG")
